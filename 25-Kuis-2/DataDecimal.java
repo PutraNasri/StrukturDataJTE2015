535731c0abe1859fc7eaab0a3643a15097a0dae0
PutraNasri <nasrisyawaldi@gmail.com>
2015-12-13 22:41:07 +0700

    kuis 2

---
+++ 25-Kuis-2/DataDecimal.java
@@ -0,0 +1,23 @@
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+/**
+ * Write a description of class DataDecimal here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class DataDecimal
+{
+    public LinkList muat(String namaBerkas){
+        BigDecimal
+        return namaBerkas.toString();
+    }
+}
